fe3e7b753f8f4f12b957c56791a8ebe6b2963f62
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-05-14 12:38:17 +0200

    stl Prim algorithm header file

--- SDIZO-Project-2/prim_mst.hpp
+++ SDIZO-Project-2/prim_mst.hpp
@@ -1,23 +1,64 @@
 #pragma once
+// #include "dynamicCollections.hpp"
+#include <list>
+#include <queue>
+#include <string>
 
 struct Edge {
 	int weight;
 	int source;
 	int destination;
+
+	inline bool operator==(const Edge& rhs) {
+		return (destination == rhs.destination && source == rhs.source && weight == rhs.weight);
+	}
+	inline bool operator!=(const Edge& rhs) {
+		return (destination != rhs.destination || source != rhs.source || weight != rhs.weight);
+	}
+	inline bool operator<(const Edge& rhs) {
+		return (weight < rhs.weight);
+	}
+	inline bool operator>(const Edge& rhs) {
+		return (weight > rhs.weight);
+	}
+	inline bool operator<=(const Edge& rhs) {
+		return (weight <= rhs.weight);
+	}
+	inline bool operator>=(const Edge& rhs) {
+		return (weight >= rhs.weight);
+	}
 };
+
+inline bool operator==(const Edge& lhs, const Edge& rhs) {
+	return (lhs.destination == rhs.destination && lhs.source == rhs.source && lhs.weight == rhs.weight);
+}
+inline bool operator<(const Edge& lhs, const Edge& rhs) {
+	return (lhs.weight < rhs.weight);
+}
+inline bool operator>(const Edge& lhs, const Edge& rhs) {
+	return (lhs.weight > rhs.weight);
+}
 
 class Prim_MST {
 public:
 	void readFromFile(std::string FileName);
 	void algorithm();
+	void displayMST();
 
 private:
-	int* verticesChecked;		// tablica sprawdzonych
-	int* verticesNotChecked;	// tablica niesprawdzonych
-	Edge* edgesCollection;
-
-	// int weight;
+	//Prim_MST();
+	//~Prim_MST();
+	/*
+	List verticesChecked;		// tablica sprawdzonych
+	List verticesNotChecked;	// tablica niesprawdzonych
+	ListEdge edgesCollection;
+	ListEdge edgesMST;
+	HeapEdge prioQueue;
+	*/
+	
+	std::list<int> verticesChecked;
+	std::list<int> verticesNotChecked;
+	std::list<Edge> edgesCollection;
+	std::list<Edge> edgesMST;
+	std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>> prioQueue;
 };
-
-
-void setEdges();
