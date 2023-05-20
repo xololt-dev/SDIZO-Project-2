2811120b66509428fa89c751c7f1ca5d22621434
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-05-20 17:49:58 +0200

    Util files for my own implementations of structures

---
+++ SDIZO-Project-2/util.cpp
@@ -0,0 +1,94 @@
+#include "util.hpp"
+#include <iostream>
+
+Matrix::Matrix() {
+	tab = NULL;
+	sideLength = 0;
+	cnt = 0;
+}
+
+Matrix::~Matrix() {	
+	clear();
+	cnt = 0;
+}
+
+void Matrix::generateEmpty(int sideLength) {
+	tab = new int* [sideLength];
+	for (int i = 0; i < sideLength; i++)
+	{
+		tab[i] = new int[sideLength];
+		for (int j = 0; j < sideLength; j++)
+		{
+			tab[i][j] = 0;
+		}
+	}
+	Matrix::sideLength = sideLength;
+}
+
+void Matrix::addValue(int x, int y, int value) {
+	tab[x][y] = value;
+	cnt++;
+}
+
+void Matrix::deleteValue(int x, int y) {
+	tab[x][y] = 0;
+	cnt--;
+}
+
+int Matrix::valueInPosition(int x, int y) {
+	return tab[x][y];
+}
+
+void Matrix::display() {
+	if (empty()) std::cout << "Macierz jest pusta." << std::endl;
+	else {
+		int lengthMST = 0;
+		int spacesNeeded = log10(sideLength) + 1;
+
+		for (int s = -1; s < spacesNeeded; s++) std::cout << " ";
+
+		for (int i = 0; i < sideLength; i++) {
+			std::cout << i << " ";
+		}
+		std::cout << "\n";
+
+		for (int s = 0; s < spacesNeeded; s++) std::cout << " ";
+		std::cout << "|";
+
+		for (int i = sideLength*2 - 1; i > 0; i--) std::cout << "-";
+		std::cout << "\n";
+
+		for (int i = 0; i < sideLength; i++) {
+			std::cout << i;
+			for (int s = 1; s < spacesNeeded; s++) std::cout << " ";
+			std::cout << "|";
+
+			for (int j = 0; j < sideLength; j++) {
+				std::cout << tab[i][j] << " ";
+				lengthMST += tab[i][j];
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\nRozmiar drzewa: " << lengthMST / 2 << "\n\n";
+	}
+}
+
+bool Matrix::empty() {
+	return !cnt;
+}
+
+void Matrix::clear() {
+	for (int i = 0; i < sideLength; i++)
+		delete[] tab[i];
+	delete[] tab;
+	tab = nullptr;
+	sideLength = 0;
+}
+
+int Matrix::size() {
+	return sideLength;
+}
+
+int Matrix::count() {
+	return cnt;
+}
---
+++ SDIZO-Project-2/util.hpp
@@ -0,0 +1,345 @@
+﻿#pragma once
+#include <climits>
+#include <iostream>
+
+struct Edge {
+	int weight;
+	int source;
+	int destination;
+
+	inline bool operator==(const Edge& rhs) {
+		return (((destination == rhs.destination && source == rhs.source) || (destination == rhs.source && source == rhs.destination)) && weight == rhs.weight);
+	}
+	inline bool operator!=(const Edge& rhs) {
+		return (destination != rhs.destination || source != rhs.source || weight != rhs.weight);
+	}
+	inline bool operator<(const Edge& rhs) {
+		return (weight < rhs.weight);
+	}
+	inline bool operator>(const Edge& rhs) {
+		return (weight > rhs.weight);
+	}
+	inline bool operator<=(const Edge& rhs) {
+		return (weight <= rhs.weight);
+	}
+	inline bool operator>=(const Edge& rhs) {
+		return (weight >= rhs.weight);
+	}
+};
+
+inline bool operator==(const Edge& lhs, const Edge& rhs) {
+	return (lhs.destination == rhs.destination && lhs.source == rhs.source && lhs.weight == rhs.weight);
+}
+inline bool operator<(const Edge& lhs, const Edge& rhs) {
+	return (lhs.weight < rhs.weight);
+}
+inline bool operator>(const Edge& lhs, const Edge& rhs) {
+	return (lhs.weight > rhs.weight);
+}
+
+struct Neighbor {
+	int weight;
+	int destination;
+	int parent;
+	int rank;
+};
+
+template <typename T> struct ListMember {
+	ListMember<T>* prev;
+	ListMember<T>* next;
+	T data;
+};
+
+class Matrix {
+public:
+	Matrix(); //konstruktor - wywoływany automatycznie przy tworzeniu obieku
+	~Matrix(); //destrukor - wywływany automatycznie przy usuwaniu obiektu
+
+	void generateEmpty(int sideLength);
+	void addValue(int x, int y, int value);
+	void deleteValue(int x, int y);
+	int valueInPosition(int x, int y);
+
+	void display();
+	bool empty();
+	void clear();
+	int size();
+	int count();
+	
+private:
+	int** tab;
+	int sideLength; // sideLength x sideLength
+	int cnt; //ilość elementów 
+};
+
+template <typename T> class List {
+public:
+	List(); //konstruktor - wywo³ywany automatycznie przy tworzeniu obieku
+	~List(); //destrukor - wyw³ywany automatycznie przy usuwaniu obiektu
+
+	ListMember<T>* front();
+	ListMember<T>* back();
+	void push_back(T value, bool increment = true);
+	void push_back(List<T>& value, bool increment = true);
+	void clear();
+	void generateEmpty(int sideLength);
+	void deleteFromList(T value);
+	int sideLength();
+	bool empty();
+
+private:
+	ListMember<T>* firstMember;
+	ListMember<T>* lastMember;
+	int cnt;
+};
+
+template <typename T> class Table {
+public:
+	Table();
+	~Table(); //destrukor - wywływany automatycznie przy usuwaniu obiektu
+
+	void addValue(int index, T value);
+	void changeValue(int index, T value, bool increment = false);
+	void deleteFromIndex(int index);
+	void generateEmpty(int sideLength);
+	void clear();
+	int length();
+	int count();
+	bool empty();
+
+private:
+	T* tab;
+	int sideLength;
+	int cnt;
+};
+
+template<typename T> List<T>::List() {
+	cnt = 0;
+	firstMember = nullptr;
+	lastMember = nullptr;
+}
+
+template<typename T> List<T>::~List() {
+	clear();
+	firstMember = nullptr;
+	lastMember = nullptr;
+}
+
+template<typename T> ListMember<T>* List<T>::front() {
+	return this->firstMember;
+}
+
+template<typename T> ListMember<T>* List<T>::back() {
+	return this->lastMember;
+}
+
+template<typename T> void List<T>::push_back(T value, bool increment) {
+	ListMember<T>* temp = new ListMember<T>;
+	
+	if (lastMember != nullptr) lastMember->next = temp;
+
+	temp->data = value;
+	temp->next = nullptr;
+	temp->prev = lastMember;
+
+	lastMember = temp;
+
+	if (firstMember == nullptr) firstMember = temp;
+
+	if (increment) cnt++;
+}
+template<typename T> void List<T>::push_back(List<T>& value, bool increment) {
+	ListMember<List<T>>* temp = new ListMember<List<T>>;
+
+	if (lastMember != nullptr) lastMember->next = temp;
+	
+	temp->data = value;
+	temp->next = nullptr;
+	temp->prev = lastMember;
+
+	lastMember = temp;
+
+	if (firstMember == nullptr) firstMember = temp;
+
+	if (increment) cnt++;
+}
+
+template<typename T> void List<T>::clear() {
+	ListMember<T>* temp;
+
+	while (firstMember != lastMember) {
+		temp = firstMember->next;
+		delete firstMember;
+		firstMember = nullptr;
+		firstMember = temp;
+	}
+	delete lastMember;
+	lastMember = nullptr;
+	firstMember = nullptr;
+}
+
+template<typename T> void List<T>::generateEmpty(int sideLength) {
+	ListMember<T>* temp = new ListMember<T>;
+
+	if (lastMember != nullptr) lastMember->next = temp;
+
+	temp->data = T{};
+	temp->next = nullptr;
+	temp->prev = lastMember;
+
+	lastMember = temp;
+
+	if (firstMember == nullptr) firstMember = temp;
+
+	for (int i = 0; i < sideLength - 1; i++) {
+		push_back(T{});
+	}
+	
+	cnt = sideLength;
+}
+
+template<typename T> void List<T>::deleteFromList(T value) {
+	// if list is empty, return with information
+	if (!cnt) return;
+
+	ListMember<T>* temp = firstMember;
+
+	for (int i = 0; i < cnt; i++) {
+		// go through each list member until value is found
+		if (temp->data != value) {
+			temp = temp->next;
+		}
+		else {
+			if (temp->prev == NULL) {
+				if (temp->next == NULL) firstMember = lastMember = NULL;
+				else {
+					temp->next->prev = NULL;
+					firstMember = temp->next;
+				}
+			}
+			else {
+				if (temp->next == NULL) {
+					lastMember = temp->prev;
+					temp->prev->next = NULL;
+				}
+				else temp->next->prev = temp->prev;
+
+				temp->prev->next = temp->next;
+			}
+
+			delete temp;
+
+			cnt--;
+
+			return;
+		}
+	}
+	// if value not found, return a message
+	std::cout << "Nie znaleziono wartosci do usuniecia!\n";
+}
+
+template<typename T> int List<T>::sideLength() {
+	return cnt;
+}
+
+template<typename T> bool List<T>::empty() {
+	return !cnt;
+}
+
+template<typename T> Table<T>::Table() {
+	tab = nullptr;
+	sideLength = 0;
+	cnt = 0;
+}
+
+template<typename T> Table<T>::~Table() {
+	clear();
+	tab = nullptr;
+}
+
+template<typename T> void Table<T>::addValue(int index, T value) {
+	T* tabTemp = new T[sideLength + 1];
+	bool passedInputValue = false;
+
+	if (index >= sideLength) tabTemp[sideLength] = value;
+	else tabTemp[index] = value;
+
+	// copying the table
+	for (int i = 0; i < sideLength; i++) {
+		if (i == index) passedInputValue = true;
+
+		tabTemp[i + passedInputValue] = tab[i];
+	}
+
+	delete[] tab;
+
+	++cnt;
+	sideLength++;
+
+	tab = tabTemp;
+}
+
+template<typename T> void Table<T>::changeValue(int index, T value, bool increment) {
+	tab[index] = value;
+	if (increment) cnt++;
+}
+
+template<typename T> void Table<T>::deleteFromIndex(int index) {
+	// if table is empty, return with message
+	if (!cnt) {
+		std::cout << "Tablica jest pusta! \n";
+		return;
+	}
+	if (index >= cnt) {
+		std::cout << "Podany indeks jest wiekszy od tablicy!\n";
+		return;
+	}
+
+	--sideLength;
+	--cnt;
+	if (!sideLength) {
+		delete[] tab;
+		tab = NULL;
+	}
+	else {
+		T* tabTemp = new T[sideLength];
+
+		// copying the values
+		int i = 0;
+		for (; i < index; i++) {
+			tabTemp[i] = tab[i];
+		}
+		for (i = index + 1; i <= cnt; i++) {
+			tabTemp[i - 1] = tab[i];
+		}
+
+		delete[] tab;
+
+		tab = tabTemp;
+	}
+}
+
+template<typename T> void Table<T>::generateEmpty(int sideLength) {
+	tab = new T[sideLength];
+	for (int i = 0; i < sideLength; i++) {
+		tab[0] = nullptr;
+	}
+	sideLength = sideLength;
+	cnt = 0;
+}
+
+template<typename T> void Table<T>::clear() {
+	delete[] tab;
+}
+
+template<typename T> int Table<T>::length() {
+	return sideLength;
+}
+
+template<typename T> int Table<T>::count() {
+	return cnt;
+}
+
+template<typename T> bool Table<T>::empty() {
+	return !cnt;
+}
