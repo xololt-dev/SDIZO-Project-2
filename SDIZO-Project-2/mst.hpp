d66153ff271aad7dd5dd28c7f525ecbc19805b88
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-05-25 18:17:57 +0200

    Class cleanup

--- SDIZO-Project-2/mst.hpp
+++ SDIZO-Project-2/mst.hpp
@@ -1,30 +1,24 @@
 #pragma once
-#include "util.hpp"
+// #include "util.hpp"
+#include "graph.hpp"
 #include <list>
 #include <queue>
 #include <string>
 
-class MST {
+class MST : public Graph {
 public:
 	// MST();
 	~MST();
 
 	void readFromFile(std::string FileName);
 	void generateGraph(int sideLength, int density);
-	std::list<std::list<int>> algorithmPrimMatrix();
-	std::list<std::list<Neighbor>> algorithmPrimList();
-	void displayMSTMatrix(std::list<std::list<int>>& matrix);
-	void displayMSTList(std::list<std::list<Neighbor>>& list);
-	void displayList();
-	void displayMatrix();
+	void algorithmPrimMatrix();
+	std::list<std::list<Neighbor>> algorithmPrimListOld();
+	void algorithmPrimList();
 
 	void addToMatrix(int src, int dst, int weight, std::list<std::list<int>>& matrix);
 	void addToList(int src, int dst, int weight, std::list<std::list<Neighbor>>& list);
 
 private:
-	// zmieniæ na tablicê [][]
-	std::list<std::list<int>> neighborMatrix;
-	std::list<std::list<Neighbor>> neighborList;
-	List<List<ListMember<Neighbor>>> myList;
-	std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>> prioQueue;
+
 };
